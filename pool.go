b1c26331d32f04768533cb53c0eb0b4c49d1ba33
Simon Zimmermann <simonz05@gmail.com>
2012-04-03 13:57:05 +0200

    use a stack as the connection pool to keep max connection count down, slower due to locking

--- pool.go
+++ pool.go
@@ -1,25 +1,64 @@
 package godis
 
+import (
+    "container/list"
+    "sync"
+)
+
 var MaxConnections = 50
 
 type connPool struct {
-    free chan *Conn
+    free chan byte
+
+    mu  sync.RWMutex
+    buf *list.List
 }
 
+// connection pool is a thread-safe stack of potential connections
+// it stores old connection. given that a caller only uses 
+// synchronous calls, only a single connection is ever opened
 func newConnPool() *connPool {
-    p := connPool{make(chan *Conn, MaxConnections)}
+    p := new(connPool)
+    p.free = make(chan byte, MaxConnections)
+    p.buf = list.New()
 
     for i := 0; i < MaxConnections; i++ {
-        p.free <- nil
+        p.free <- 'c'
     }
 
-    return &p
+    return p
 }
 
+// pop will block until a connection is available
+// returns *Conn, or nil if connection is yet not 
+// created
 func (p *connPool) pop() *Conn {
-    return <-p.free
+    // wait for semaphore
+    <-p.free
+
+    // aquire lock
+    p.mu.Lock()
+    defer p.mu.Unlock()
+
+    // conn is not created
+    if p.buf.Len() == 0 {
+        return nil
+    }
+
+    e := p.buf.Front()
+    t, _ := e.Value.(*Conn)
+    p.buf.Remove(e)
+    return t
 }
 
 func (p *connPool) push(c *Conn) {
-    p.free <- c
+    // aquire lock
+    p.mu.Lock()
+    defer p.mu.Unlock()
+
+    if c != nil {
+        p.buf.PushFront(c)
+    }
+
+    p.free <- 'c'
 }
